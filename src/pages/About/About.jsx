5a7eb6218cd3552d045e1fcfe984cb80ec1bb282
Dushyant Pratap Singh <singhdushyant2901@gmail.com>
2023-07-26 09:19:05 +0530

    feat: error and about page

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,5 +1,36 @@
 import React from "react";
+import { SectionTitle } from "../../components";
 
 export const About = () => {
-  return <div className="bg-green-400 w-2/3 h-2/3">About</div>;
+  return (
+    <main>
+      <SectionTitle title="About" />
+      <p className="text-lg w-2/3 mx-auto text-center">
+        {" "}
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio eos
+        commodi itaque! Impedit ipsa voluptatum adipisci necessitatibus,
+        laboriosam dicta, ullam, laudantium soluta non nulla nisi earum esse
+        fugiat vitae odio aspernatur voluptate rerum accusamus dolore? Aliquam
+        quibusdam suscipit magni, eaque reprehenderit aut, illum hic placeat
+        tenetur quis pariatur quasi debitis. Commodi nisi neque assumenda
+        cumque, ipsam, soluta eveniet rem accusantium eius nesciunt veritatis
+        deserunt quibusdam libero, non a sequi sunt sit similique cum earum
+        impedit explicabo! Quod doloremque voluptate tempora. Corporis commodi
+        sapiente quam libero perferendis impedit saepe deserunt aut nobis, quo
+        aperiam odit quasi voluptatem est laudantium aliquid fuga praesentium.
+        Incidunt voluptate voluptatum consequatur tenetur architecto in
+        voluptates dolore facere natus maiores. Blanditiis explicabo optio, aut
+        sed dolore vero maxime cumque repudiandae aliquid, quas aspernatur
+        dolorum consectetur dolorem eum. Laborum fugit aliquid ad, voluptatibus
+        rem animi libero dicta molestiae magnam possimus quod incidunt?
+        Explicabo itaque doloribus facilis illo exercitationem doloremque
+        consequuntur? Numquam quae nulla temporibus molestias mollitia
+        molestiae. Quisquam sed distinctio aliquam error beatae velit, a sequi
+        earum? Fugiat, accusamus maiores id quidem blanditiis debitis rerum amet
+        ab. Molestias impedit quae ut laborum omnis soluta doloribus
+        dignissimos, perspiciatis cupiditate, porro ad sequi quas adipisci
+        facere corrupti itaque necessitatibus minima?
+      </p>
+    </main>
+  );
 };
