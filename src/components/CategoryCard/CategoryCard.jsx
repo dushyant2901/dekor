5ec34890bf7cf93097e74c8c12ecf7039ca2f8c1
Dushyant Pratap Singh <singhdushyant2901@gmail.com>
2023-07-26 09:16:11 +0530

    fix: remove unused components

--- src/components/CategoryCard/CategoryCard.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const CategoryCard = () => {
-  return (
-    <article className="w-52 h-52 bg-purple-200 flex items-center">
-      CategoryCard
-    </article>
-  );
-};
